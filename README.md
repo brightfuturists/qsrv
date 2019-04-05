e1704bb940b3187fbbab5d19d86321e4bb41a129
Bright Futurists <brightfuturists@gmail.com>
2019-04-05 19:23:37 +0000

    Final readme information from Github

---
+++ README.md
@@ -0,0 +1,3 @@
+# qsrv
+
+`curl https://raw.githubusercontent.com/brightfuturists/qsrv/master/index.js -o index.js`
--- index.js
+++ index.js
@@ -1,38 +1,45 @@
-/*
+/***
 
 Setup & Usage:
 
 1) Install Node.js for Linux (login as `root` if Debian):
+
 curl -sL https://deb.nodesource.com/setup_11.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
-2) Install qsrv:
+2) Fetch & Install qsrv:
+
+mkdir qsrv && cd qsrv
+curl https://raw.githubusercontent.com/brightfuturists/qsrv/master/index.js -o index.js
 npm i
 
-2) Place files and directories into `qsrv/files`:
-cp file.txt qsrv/files/
+3) Place files and directories into `qsrv/files`:
 
-2) Run your server:
-node qsrv/index.js
+cp ~/myfile.txt files/
 
-3) Use your server:
-http://localhost:80/file.txt
+4) Run your server:
 
-or
+node index.js
+
+5) Use your server:
+
+Visit http://localhost:80/file.txt
+
+or,
 
 curl http://localhost:80/file.txt -o file.txt
 
-(In most use cases, a Domain or an IP would be in place of `localhost`)
+(In most use-cases, a Domain or an IP would be in place of `localhost`)
 
-*/
+***/
 
 const express = require('express')
 const app = express()
 const port = 80
 
-app.use(express.static(__dirname + '/files', { dotfiles: 'allow' } ))
+app.use(express.static(__dirname + '/files', {dotfiles: 'allow'}))
 
 app.listen(port, () => {
-  console.log('HTTP server running on port 80')
+  console.log(`HTTP server running on port ${port}`)
 })
 
